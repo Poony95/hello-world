0d139313e67b21e1ccf0373bae21c437948f0a7b
mycom <mycom@DESKTOP-ATA8RH0>
2022-08-02 22:22:33 +0900

    my name is Ryu 메시지 추가

--- src/main.java
+++ src/main.java
@@ -2,7 +2,8 @@
 public class main {
 	public static void main(String[] args) {
 		System.out.println("hellow world");
-		System.out.println("Hi hahaha")
+		System.out.println("Hi hahaha");
+		System.out.println("my name is Ryu");
 	}
 }
 
